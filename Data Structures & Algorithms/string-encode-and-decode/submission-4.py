78620d4c44d293bccb72aed48517908ecb5abf15
Pragya <77471889+Pragya1904@users.noreply.github.com>
2026-08-23 18:53:32 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        output = "*/*".join(strs) if len(strs) > 0 else "-1"
+        print(len(strs))
+        print(output)
+        return output
+    def decode(self, s: str) -> List[str]:
+        return s.split("*/*") if s != "-1" else []
